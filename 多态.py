3199505d17223ffbe199de9704adb811e842d9ca
mac <mac@macdeMacBook-Pro.local>
2023-05-09 20:32:31 +0800

    add

---
+++ 多态.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+class Person():
+    def eat(self):
+        print('人，喜欢吃五谷杂粮')
+
+class Cat():
+    def eat(self):
+        print('猫，喜欢吃鱼')
+
+class Dog():
+    def eat(self):
+        print('狗，喜欢啃骨头')
+
+def fun(obj):   #函数的定义处，obj是函数的形式参数
+    obj.eat()   #对象名打点调用eat方法
+
+if __name__ == '__main__':
+    person=Person() #创建Person类型的对象person
+    cat=Cat()       #创建Cat类型的对象cat
+    dog=Dog()       #创建Dog类型的对象dog
+
+    #调用fun函数
+    fun(person)
+    fun(cat)
+    fun(dog)
